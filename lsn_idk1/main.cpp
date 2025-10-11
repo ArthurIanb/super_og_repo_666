99f0d30ddeb332b56f606a5dddaa1d83b8361130
ArthurIanb <artur.ianb@gmail.com>
2025-10-11 10:32:05 +0300

    second_zadacha+first

---
+++ lsn_idk1/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+// #include <cmath>
+
+
+int main(){
+    // int n;
+    // std::cin >> n;
+    // for(int i = 1; i <= n; ++i){
+    //     if(i % 3 == 0 && i % 5 == 0){
+    //         std::cout << i << " foobar\n";
+    //     }else if(i % 3 == 0)
+    //         std::cout << i << " foo\n";
+    //     else if(i % 5 == 0)
+    //         std::cout << i << " bar\n";
+    // }
+
+
+    // long long int x, n;
+    // std::cin >> x >> n;
+    // int out;
+    // bool defined_min_num = false;
+    // long long int min_num = 0;
+    // for(int i = 0; i < n; i++){
+    //     int k;
+    //     std::cin >> k;
+    //     if(!defined_min_num){
+    //         min_num = std::abs(x - k);
+    //         defined_min_num = true;
+    //     }
+    //     if(std::abs(x - k) == min_num && k < out){
+    //         out = k;
+    //     }
+    //     if(std::abs(x - k) < min_num){
+    //         out = k;
+    //         min_num = std::abs(x - k);
+    //     }
+    // }
+    // std::cout << out;
+}

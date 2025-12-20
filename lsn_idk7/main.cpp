054aa87784ea3a074807e7ccd6e01beb32ece330
ArthurIanb <artur.ianb@gmail.com>
2025-12-20 12:01:52 +0300

    2012

---
+++ lsn_idk7/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+
+
+int main(){
+    int arr[3][3] = {{1, 2}, {}, {1, 2, 3}};
+    std::cout << arr[0,1];
+    std::cout << '\n' << 1, 3;
+    char str[6] = "hello";
+    std::cout << '\n' << str;
+}
